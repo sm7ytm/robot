984dd969582540609fbe052d962697127e7b030b
Magnus Jönsson <sm7ytm@gmail.com>
2018-06-22 10:47:20 +0200

    Uppdaterat med kortens adresser och anpassat simpletest.py
    Provkört med två kort och ett servo på vardera kortet och det funkar.

--- simpletest.py
+++ simpletest.py
@@ -14,14 +14,15 @@ import Adafruit_PCA9685
 #logging.basicConfig(level=logging.DEBUG)
 
 # Initialise the PCA9685 using the default address (0x40).
-pwm = Adafruit_PCA9685.PCA9685()
+#pwm = Adafruit_PCA9685.PCA9685()
 
 # Alternatively specify a different address and/or bus:
-#pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
+left = Adafruit_PCA9685.PCA9685(address=0x40)
+right = Adafruit_PCA9685.PCA9685(address=0x41)
 
 # Configure min and max servo pulse lengths
-servo_min = 150  # Min pulse length out of 4096
-servo_max = 600  # Max pulse length out of 4096
+servo_min = 250  # Min pulse length out of 4096
+servo_max = 450  # Max pulse length out of 4096
 
 # Helper function to make setting a servo pulse width simpler.
 def set_servo_pulse(channel, pulse):
@@ -35,12 +36,18 @@ def set_servo_pulse(channel, pulse):
     pwm.set_pwm(channel, 0, pulse)
 
 # Set frequency to 60hz, good for servos.
-pwm.set_pwm_freq(60)
+left.set_pwm_freq(60)
+right.set_pwm_freq(60)
 
 print('Moving servo on channel 0, press Ctrl-C to quit...')
 while True:
     # Move servo on channel O between extremes.
-    pwm.set_pwm(0, 0, servo_min)
+    left.set_pwm(1, 0, servo_min)
     time.sleep(1)
-    pwm.set_pwm(0, 0, servo_max)
+    left.set_pwm(1, 0, servo_max)
+    time.sleep(2)
+
+    right.set_pwm(2, 0, servo_min)
     time.sleep(1)
+    right.set_pwm(2, 0, servo_max)
+    time.sleep(2)
