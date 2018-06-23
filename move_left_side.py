1621c3589da400cad9167d197625e9a3e4599b82
Magnus Jönsson <sm7ytm@gmail.com>
2018-06-23 15:00:59 +0200

    Tagit ut funktionen move i förberedande för namnändring

--- move_left_side.py
+++ move_left_side.py
@@ -1,22 +1,4 @@
-from init import *
-from ben import *
-
-def move(sekvens):
-
-    side, port, start, stop = styrkod[sekvens]
-
-    
-    if (side == 'l'):
-        left.set_pwm(port, start, stop)
-    
-    elif (side == 'r'):
-        right.set_pwm(port, start, stop)
-    
-    else:
-        pass
-    
-    
-
+from functions import move
 
 move('VFVC')
 time.sleep(1)
